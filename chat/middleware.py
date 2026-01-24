37b31c54f16189c32dcde28b8e9f310db74edc8e
Sangeeta-g <sangeetadani599@gmail.com>
2026-01-24 13:26:26 +0530

    changes added

--- chat/middleware.py
+++ chat/middleware.py
@@ -1,18 +1,25 @@
 from urllib.parse import parse_qs
 from django.contrib.auth.models import AnonymousUser
-from rest_framework_simplejwt.authentication import JWTAuthentication
 from channels.middleware import BaseMiddleware
+from channels.db import database_sync_to_async
 
 class JWTAuthMiddleware(BaseMiddleware):
+
+    @database_sync_to_async
+    def get_user(self, token):
+        from rest_framework_simplejwt.authentication import JWTAuthentication
+
+        jwt_auth = JWTAuthentication()
+        validated = jwt_auth.get_validated_token(token)
+        return jwt_auth.get_user(validated)
+
     async def __call__(self, scope, receive, send):
         query_string = scope.get("query_string", b"").decode()
-        token = parse_qs(query_string).get("token")
+        token_list = parse_qs(query_string).get("token")
 
-        if token:
+        if token_list:
             try:
-                jwt_auth = JWTAuthentication()
-                validated = jwt_auth.get_validated_token(token[0])
-                scope["user"] = jwt_auth.get_user(validated)
+                scope["user"] = await self.get_user(token_list[0])
             except Exception:
                 scope["user"] = AnonymousUser()
         else:
