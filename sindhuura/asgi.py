5ae91d6bf077ec9c85bd7e7e7ab11e3d26dd4531
Sangeeta-g <sangeetadani599@gmail.com>
2026-01-24 13:56:35 +0530

    added asgi

--- sindhuura/asgi.py
+++ sindhuura/asgi.py
@@ -2,21 +2,22 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sindhuura.settings")
 
 from django.core.asgi import get_asgi_application
-
-# ✅ FIRST: fully initialize Django
 django_asgi_app = get_asgi_application()
 
-# ✅ ONLY AFTER Django is ready
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
+from channels.security.websocket import OriginValidator
 from chat.middleware import JWTAuthMiddleware
 import chat.routing
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    "websocket": AllowedHostsOriginValidator(
+    "websocket": OriginValidator(
         JWTAuthMiddleware(
             URLRouter(chat.routing.websocket_urlpatterns)
-        )
+        ),
+        [
+            "https://admin.sindhuura.com",
+            "http://admin.sindhuura.com",
+        ]
     ),
 })
