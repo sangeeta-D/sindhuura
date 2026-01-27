c0c73dd97912bd23673cc69f83a8607e61248c10
Sangeeta-g <sangeetadani599@gmail.com>
2026-01-27 10:47:13 +0530

    added asgi

--- sindhuura/asgi.py
+++ sindhuura/asgi.py
@@ -1,11 +1,19 @@
 import os
+import django
+
+# Set the settings module FIRST
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sindhuura.settings")
 
+# Initialize Django BEFORE importing anything else
+django.setup()
+
+# Now import Django and Channels components
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from chat.middleware import JWTAuthMiddleware
 import chat.routing
 
+# Get the Django ASGI application
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
